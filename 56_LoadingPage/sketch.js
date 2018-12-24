22df60d4626dbe705e965fa062a8cab5ce85234d
alcaloke <35482173+alcaloke@users.noreply.github.com>
2018-12-24 12:10:40 +0900

    Add files via upload

---
+++ 56_LoadingPage/sketch.js
@@ -0,0 +1,41 @@
+let h = 400;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	bauhaus = loadFont('BauhausStd-Medium.otf');
+}
+
+let ef = 0;
+let efs = 2;
+let tf = 255;
+let tfs = 1;
+
+function draw() {
+	loadingPage();
+}
+
+function loadingPage() {
+	background(0);
+	noStroke();
+
+	fill(tf);
+	ellipse(width / 2, height / 2, 150);
+
+	fill(ef);
+	ellipse(width / 2, height / 2, 130);
+
+	fill(tf);
+	textFont(bauhaus);
+	textSize(34);
+	textAlign(CENTER);
+	text('Loading', width/2, height/2 + 10);
+
+	ef += efs;
+	if (ef > 255 | ef < 0) {
+		efs *= -1;
+	}
+	tf -= tfs;
+	if (tf > 255 | tf < 0) {
+		tfs *= -1;
+	}
+}
